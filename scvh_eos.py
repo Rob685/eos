c0bc7748d595799e8e9d3053e661491adfab48ba
Rob685 <arevalo@princeton.edu>
2023-05-24 09:40:45 -0400

    scvh_eos changes

--- scvh_eos.py
+++ scvh_eos.py
@@ -124,7 +124,7 @@ def get_rhos_p_t(p, t, y):
 
 ###### derivatives ######
 
-s_arr, p_arr, t_arr, r_arr, y_arr, cp_arr, cv_arr, chirho_arr, chit_arr, gamma1_arr, grada_arr = np.load('/Users/Helios/planet_interiors/state/scvh/scvh_thermo.npy')
+s_arr, p_arr, t_arr, r_arr, y_arr, cp_arr, cv_arr, chirho_arr, chit_arr, gamma1_arr, grada_arr = np.load('eos/scvh/scvh_thermo.npy')
 
 get_rho = RGI((y_arr[:,0][:,0], s_arr[0][:,0], p_arr[0,:][0]), r_arr, method='linear', bounds_error=False, fill_value=None)
 get_t= RGI((y_arr[:,0][:,0], s_arr[0][:,0], p_arr[0,:][0]), t_arr, method='linear', bounds_error=False, fill_value=None)
