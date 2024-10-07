fixing ds sign in metals_eos

metals_eos.py
@@ -257,7 +257,7 @@ def get_c_v(s, rho, eos, f_ppv, f_fe, ds=1e-3):
 def get_c_p(s, p, eos, f_ppv, f_fe, ds=1e-3):
     # ds/dlogT_{P, Y}
 
-    lgt2 = get_t_sp_tab(s - ds, p, eos, f_ppv=f_ppv, f_fe=f_fe)
-    lgt1 = get_t_sp_tab(s + ds, p, eos, f_ppv=f_ppv, f_fe=f_fe)
+    lgt2 = get_t_sp_tab(s + ds, p, eos, f_ppv=f_ppv, f_fe=f_fe)
+    lgt1 = get_t_sp_tab(s - ds, p, eos, f_ppv=f_ppv, f_fe=f_fe)
  
     return (2 * ds / erg_to_kbbar)/((lgt2 - lgt1) * np.log(10))